9051bff4e2c056249b6432de3073a20c31b6074c
Tanmay Deshpande <tanmayd@satvix.com>
2019-03-20 12:21:24 +0530

    Tanmay | Expiry Date reflected in End of Life Date and Best Before Date

--- bahmni_stock/models/stock_pack_operation_lot.py
+++ bahmni_stock/models/stock_pack_operation_lot.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+import logging
 from datetime import datetime
 
 from odoo import models, fields, api, _
 from odoo.exceptions import Warning
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
-
+logger = logging.getLogger(__name__)
 
 class StockPackOperation(models.Model):
     _inherit = 'stock.pack.operation'
@@ -22,9 +23,14 @@ class StockPackOperation(models.Model):
         for pack in self:
             if pack.product_id.tracking != 'none':
                 for lot in pack.pack_lot_ids:
-                    life_date = datetime.strptime(lot.lot_id.life_date, DTF)
-                    if life_date < datetime.today():
-                        raise Warning("Lot %s is expired, you can process expired lot!"%(lot.lot_id.name))
+                    logger.info("LOT LIFE DATE")
+                    logger.info(lot.lot_id.life_date)
+                    if lot.expiry_date and lot.lot_id:
+                        lot.lot_id.life_date = lot.expiry_date
+                        lot.lot_id.use_date = lot.expiry_date
+                    #life_date = datetime.strptime(lot.lot_id.life_date, DTF)
+                    #if life_date < datetime.today():
+                        #raise Warning("Lot %s is expired, you can process expired lot!"%(lot.lot_id.name))
                 pack.write({'qty_done': sum(pack.pack_lot_ids.mapped('qty'))})
         return {'type': 'ir.actions.act_window_close'}
 
@@ -34,17 +40,18 @@ class StockPackOperationLot(models.Model):
 
     available_qty = fields.Integer(string="Available Qty")
     
-    @api.onchange('lot_id')
-    def onchange_lot_id(self):
-        if self.lot_id:
-            if self.lot_id.life_date:
-                lot_life_date = datetime.strptime(self.lot_id.life_date, DTF)
-                if lot_life_date < datetime.today():
-                    self.expiry_date = self.lot_id.life_date
-                    warning = {'name': 'Lot Expired',
-                               'message': "This lot is already expired!"}
-                    return {'warning': warning}
-            self.available_qty = self.env['product.product'].with_context({'location': self.operation_id.location_id.id,
-                                                                           'lot_id': self.lot_id.id}).browse(self.operation_id.product_id.id).qty_available
-            self.expiry_date = self.lot_id.life_date
+    #@api.onchange('lot_id')
+    #def onchange_lot_id(self):
+        #if self.lot_id:
+            #if self.lot_id.life_date:
+                #lot_life_date = datetime.strptime(self.lot_id.life_date, DTF)
+                #if lot_life_date < datetime.today():
+                    #self.expiry_date = self.lot_id.life_date
+                    #warning = {'name': 'Lot Expired',
+                               #'message': "This lot is already expired!"}
+                    #return {'warning': warning}
+            #self.available_qty = self.env['product.product'].with_context({'location': self.operation_id.location_id.id,
+            #'lot_id': self.lot_id.id}).browse(self.operation_id.product_id.id).qty_available
+            #self.expiry_date = self.lot_id.life_date
             
+         
