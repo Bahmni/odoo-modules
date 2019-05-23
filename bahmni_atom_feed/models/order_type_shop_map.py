f3625a977fed7378a7d77e053dd436cd5e0e4ef3
Anand Patel <flt.anandkumar@gmail.com>
2019-05-23 14:26:16 +0530

    [BAH-824]Change Location from 'string' to a 'reference in location' in 'Order type - shop mapping'.

--- bahmni_atom_feed/models/order_type_shop_map.py
+++ bahmni_atom_feed/models/order_type_shop_map.py
@@ -7,7 +7,7 @@ class order_type_shop_map(models.Model):
 
     order_type = fields.Many2one('order.type','Order Type', required=True)
     shop_id = fields.Many2one('sale.shop', 'Shop')
-    location_name = fields.Char('Location Name')
+    location_id = fields.Many2one('stock.location','Location Name')
     local_shop_id = fields.Many2one('sale.shop', 'Local Shop')
 
 
