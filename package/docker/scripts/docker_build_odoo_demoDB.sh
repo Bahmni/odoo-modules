e9f31837664abb8d3155afcb359127c24c36995e
MOHANKUMAR T <mohan13081999@gmail.com>
2022-02-23 16:52:53 +0530

    BAH-1413 | refactor. split odoo image build into a seperate workflow
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

---
+++ package/docker/scripts/docker_build_odoo_demoDB.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xe
+
+#Fetching Database Backup Data
+gunzip -f -k ../bahmni-scripts/demo/db-backups/v0.92/odoo_backup.sql.gz
+cp ../bahmni-scripts/demo/db-backups/v0.92/odoo_backup.sql package/resources/odoo_demo_dump.sql
+
+#Building Docker images
+cd package
+ODOO_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
+docker build -t bahmni/odoo-10-db:demo-${ODOO_IMAGE_TAG} -f docker/demodb.Dockerfile  . --no-cache
