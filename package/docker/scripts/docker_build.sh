6f6c6cc60d7d7c865f1b0710f08f76db540873e9
MOHANKUMAR T <mohan13081999@gmail.com>
2022-02-23 17:31:53 +0530

    BAH-1413 | refactor. removed unified build and publish script
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

--- package/docker/scripts/docker_build.sh
+++ package/docker/scripts/docker_build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -xe
 
-#Fetching Database Backup Data
-gunzip -f -k bahmni-scripts/demo/db-backups/v0.92/odoo_backup.sql.gz
-cp bahmni-scripts/demo/db-backups/v0.92/odoo_backup.sql package/resources/odoo_demo_dump.sql
-
-cd package
-# Unzipping Odoo Modules copied by CI
-mkdir -p build/odoo-modules
-unzip -q -u -d build/odoo-modules resources/odoo-modules.zip
-#Building Docker images
-ODOO_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
-docker build -t bahmni/odoo-10-db:fresh-${ODOO_IMAGE_TAG} -f docker/db.Dockerfile  . --no-cache
-docker build -t bahmni/odoo-10-db:demo-${ODOO_IMAGE_TAG} -f docker/demodb.Dockerfile  . --no-cache
-docker build -t bahmni/odoo-10:${ODOO_IMAGE_TAG} -f docker/Dockerfile  . --no-cache
+#This Script can be used to build all three images of Odoo
+package/docker/scripts/docker_build_odoo_demoDB.sh
+package/docker/scripts/docker_build_odoo_freshDB.sh
+package/docker/scripts/docker_build_odoo.sh
