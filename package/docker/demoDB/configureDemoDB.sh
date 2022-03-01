f2c5a9f95a8148cafdf2e6eb0bbaf479b0e0d9e7
MOHANKUMAR T <mohan13081999@gmail.com>
2022-03-01 15:48:14 +0530

    BAH-1413 | refactored. demo DB build workflow to use docker build&push action
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

--- package/docker/demoDB/configureDemoDB.sh
+++ package/docker/demoDB/configureDemoDB.sh
@@ -2,4 +2,4 @@
 
 cd /resources/
 psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f setupDB.sql &> /dev/null
-psql -U ${POSTGRES_USER} -d odoo -f odoo_demo_dump.sql &> /dev/null
+psql -U ${POSTGRES_USER} -d odoo -f odoo_backup.sql &> /dev/null
